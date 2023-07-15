4429829fe5714ceef4e108282d1050a7bb9c6cb2
bartlomiej.mojek <bartlomiej.mojek@PK6-08>
2023-07-15 15:57:08 +0200

    Dodaj pliki projektów.

---
+++ Lab5/Controllers/TaskController.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Lab5.Controllers
+{
+    public class TaskController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        private readonly TaskManager _taskManager;
+
+        public TaskController()
+        {
+            _taskManager = new TaskManager();
+        }
+
+        [HttpGet]
+        public IActionResult GetAllTasks()
+        {
+            var tasks = _taskManager.GetTasks();
+            return Ok(tasks);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetTaskById(int id)
+        {
+            var task = _taskManager.GetTaskById(id);
+            if (task != null)
+                return Ok(task);
+
+            return NotFound();
+        }
+
+        [HttpPost]
+        public IActionResult CreateTask(Task task)
+        {
+            _taskManager.AddTask(task);
+            return CreatedAtAction(nameof(GetTaskById), new { id = task.Id }, task);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult UpdateTask(int id, Task updatedTask)
+        {
+            var result = _taskManager.UpdateTask(id, updatedTask);
+            if (result)
+                return NoContent();
+
+            return NotFound();
+        }
+
+        [HttpPatch("{id}/complete")]
+        public IActionResult MarkTaskAsCompleted(int id)
+        {
+            var result = _taskManager.MarkTaskAsCompleted(id);
+            if (result)
+                return NoContent();
+
+            return NotFound();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult DeleteTask(int id)
+        {
+            var result = _taskManager.DeleteTask(id);
+            if (result)
+                return NoContent();
+
+            return NotFound();
+        }
+    }
+}
---
+++ TestProject1/TaskManagerTest.cs
@@ -0,0 +1,53 @@
+﻿using Lab5;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Task = Lab5.Task;
+
+namespace TestProject1
+{
+    public class TaskManagerTest
+    {
+
+        private TaskManager _taskManager;
+        [SetUp]
+        public void Setup()
+        {
+            _taskManager = new TaskManager();
+        }
+        [Test]
+        public void AddTask_ShouldIncreaseTaskCount()
+        {
+            // Arrange
+            var task = new Task("Test task");
+            // Act
+            _taskManager.AddTask(task);
+            // Assert
+            Assert.AreEqual(1, _taskManager.GetTasks().Count);
+        }
+        [Test]
+        public void RemoveTask_ExistingTask_ShouldDecreaseTaskCount()
+        {
+            var task = new Task("Zadanie");
+            _taskManager.AddTask(task);
+
+            int TaskCount = _taskManager.GetTasks().Count;
+
+            _taskManager.RemoveTask(task.Id);
+
+            Assert.AreEqual(TaskCount - 1, _taskManager.GetTasks().Count);
+        }
+        [Test]
+        public void RemoveTask_NonExistingTask_ShouldNotChangeTaskCount()
+        {
+            var task = new Task("Zadanie");
+            _taskManager.AddTask(task);
+
+            _taskManager.RemoveTask(task.Id+1);
+
+            Assert.AreEqual(_taskManager.GetTasks().Count, 1);
+        }
+    }
+}
